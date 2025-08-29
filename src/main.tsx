ebcc64f18d67b1760adefc9388135f0cab9a2170
Christian <christiandave120702@gmail.com>
2025-08-29 18:15:54 +0800

    feat: implement the custom middleware for each pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,80 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import Dashboard from './pages/Dashboard.tsx'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import Home from "./layout/Home.tsx";
+import Dashboard from "./pages/Dashboard.tsx";
+import InventoryList from "./pages/InventoryList.tsx";
+import ItemList from "./pages/ItemList.tsx";
+import Staff from "./pages/Staff.tsx";
+import HistoryList from "./pages/HistoryList.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { PublicRoute, ProtectedRoute } from "./utils/middleware/accessAuth.tsx";
 
 const routes = createBrowserRouter([
-  { path: '/', element: <App /> },
-  { path: '/dashboard', element: <Dashboard /> }
-])
+  {
+    path: "/",
+    element: (
+      <PublicRoute>
+        <App />
+      </PublicRoute>
+    ),
+  },
+  {
+    path: "/home",
+    element: <Home />,
+    children: [
+      {
+        path: "dashboard",
+        element: (
+          <ProtectedRoute>
+            <Dashboard />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: "inventory-list",
+        element: (
+          <ProtectedRoute>
+            <InventoryList />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: "item-list",
+        element: (
+          <ProtectedRoute>
+            <ItemList />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: "staff",
+        element: (
+          <ProtectedRoute>
+            <Staff />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: "history-list",
+        element: (
+          <ProtectedRoute>
+            <HistoryList />
+          </ProtectedRoute>
+        ),
+      },
+    ],
+  },
+]);
 
-createRoot(document.getElementById('root')!).render(
+const queryClient = new QueryClient();
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={routes} />
-  </StrictMode>,
-)
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={routes} />
+    </QueryClientProvider>
+  </StrictMode>
+);
