efbf27087ca597296e24eda805c4266fa13bd1a1
Christian <christiandave120702@gmail.com>
2025-09-09 11:12:40 +0800

    chore: update the User register type

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,9 +1,9 @@
 export type TRegisterUser = {
-  first_name: string;
-  last_name: string;
-  username: string;
-  password: string;
-  confirm_password: string;
+  firstName: string,
+  lastName: string,
+  username: string,
+  password: string,
+  confirmPassword: string
 };
 
 export type TLoginUser = {
