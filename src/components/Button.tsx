0e09dc84246f26930f756ae234622b6d0fc8b314
Christian <christiandave120702@gmail.com>
2025-09-17 12:48:22 +0800

    feat: create a reusable Button Component

---
+++ src/components/Button.tsx
@@ -0,0 +1,20 @@
+import { FaPlus } from "react-icons/fa6"
+
+
+type ButtonProps = {
+    onClick?: () => void;
+    name?: string;
+}
+
+
+export default function Button({ onClick, name }: ButtonProps) {
+    return (
+        <button
+            className="flex items-center gap-2 px-5 py-3 cursor-pointer bg-gradient-to-r from-[#2563eb] to-[#38bdf8] text-white font-semibold rounded-xl shadow-lg hover:scale-105 hover:shadow-2xl transition-all duration-150"
+            type="button"
+            onClick={onClick}
+        >
+            <FaPlus /> {name}
+        </button>
+    )
+}
