3cf97793ca0fe0f1d3f6a48f9e65ab1b1c809f62
Christian <christiandave120702@gmail.com>
2025-09-17 12:48:41 +0800

    feat: create a reusable Close Button Component

---
+++ src/components/CloseButton.tsx
@@ -0,0 +1,11 @@
+import { FaPlus } from "react-icons/fa6";
+
+interface CloseButtonProps {
+    onClick: () => void;
+}
+
+export default function CloseButton({ onClick }: CloseButtonProps) {
+    return (
+        <FaPlus onClick={onClick} className="transform rotate-45 transition-all duration-200 text-2xl text-gray-400 cursor-pointer w-[32px] h-[32px] items-center justify-center rounded-full hover:text-white hover:bg-red-500 hover:rotate-180" />
+    )
+}
