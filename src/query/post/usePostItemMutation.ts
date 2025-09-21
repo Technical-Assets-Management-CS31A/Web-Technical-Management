15b019fec40173a85a43f05120c536fe24b11f49
Christian <christiandave120702@gmail.com>
2025-09-21 11:47:27 +0800

    feat: create a post item mutation  using Tanstack Query

---
+++ src/query/post/usePostItemMutation.ts
@@ -0,0 +1,25 @@
+import { useMutation } from "@tanstack/react-query";
+import type { TItemForm } from "../../types/types";
+import { getToken } from "../../utils/token";
+
+const PostItem = async (formData: TItemForm) => {
+  const BASE_URL = import.meta.env.VITE_CREATE_ITEM_API;
+  const res = await fetch(BASE_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${getToken()}`,
+    },
+    body: JSON.stringify(formData),
+    credentials: "include",
+  });
+  if (!res.ok) throw new Error("Submission failed");
+  return res.json();
+};
+
+// Custom hook for posting an item
+export const usePostItemMutation = () => {
+  return useMutation({
+    mutationFn: PostItem,
+  });
+};
