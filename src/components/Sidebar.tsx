f6c2cfd349157f7ce09dc0d379e0e61f8272e054
Christian <christiandave120702@gmail.com>
2025-09-21 11:56:01 +0800

    feat: implement sidebar loading state with SidebarSkeletonLoader and refactor navigation items into a list

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -3,14 +3,36 @@ import logo from "../assets/img/aclcLogo.webp";
 import { CiLogout } from "react-icons/ci";
 import { removeToken } from "../utils/token";
 import { useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+import SidebarSkeletonLoader from "../loader/SidebarSkeletonLoader";
+
+const sideBarList = [
+    { label: "Dashboard", link: "dashboard" },
+    { label: "Inventory List", link: "inventory-list" },
+    { label: "Your Staff", link: "staff" },
+    { label: "Your History", link: "history-list" },
+]
 
 export default function Sidebar() {
     const navigate = useNavigate();
+    const [isSidebarLoading, setIsSidebarLoading] = useState(true);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setIsSidebarLoading(false);
+        }, 1500);
+        return () => clearTimeout(timer);
+
+    }, []);
 
     const logoutUser = async () => {
         await removeToken();
         navigate("/");
     };
+
+    if (isSidebarLoading) {
+        return <SidebarSkeletonLoader />
+    }
 
     return (
         <aside className="animate-fadeIn h-screen w-[300px] bg-white border-r border-[#e5e7eb] flex flex-col justify-between shadow-xl left-0 top-0 z-30">
@@ -22,58 +44,21 @@ export default function Sidebar() {
             {/* Navigation */}
             <nav className="flex-1">
                 <ul className="flex flex-col gap-2 px-4">
-                    <li>
-                        <NavLink
-                            to="dashboard"
-                            className={({ isActive }) =>
-                                `flex items-center gap-3 px-4 py-3 rounded-lg font-medium text-base transition-all duration-150 ${isActive
-                                    ? "bg-[#2563eb] text-white shadow"
-                                    : "text-[#334155] hover:bg-[#f1f5f9] hover:text-[#2563eb]"
-                                }`
-                            }
-                        >
-                            <span>Dashboard</span>
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink
-                            to="inventory-list"
-                            className={({ isActive }) =>
-                                `flex items-center gap-3 px-4 py-3 rounded-lg font-medium text-base transition-all duration-150 ${isActive
-                                    ? "bg-[#2563eb] text-white shadow"
-                                    : "text-[#334155] hover:bg-[#f1f5f9] hover:text-[#2563eb]"
-                                }`
-                            }
-                        >
-                            <span>Inventory List</span>
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink
-                            to="staff"
-                            className={({ isActive }) =>
-                                `flex items-center gap-3 px-4 py-3 rounded-lg font-medium text-base transition-all duration-150 ${isActive
-                                    ? "bg-[#2563eb] text-white shadow"
-                                    : "text-[#334155] hover:bg-[#f1f5f9] hover:text-[#2563eb]"
-                                }`
-                            }
-                        >
-                            <span>Your Staff</span>
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink
-                            to="history-list"
-                            className={({ isActive }) =>
-                                `flex items-center gap-3 px-4 py-3 rounded-lg font-medium text-base transition-all duration-150 ${isActive
-                                    ? "bg-[#2563eb] text-white shadow"
-                                    : "text-[#334155] hover:bg-[#f1f5f9] hover:text-[#2563eb]"
-                                }`
-                            }
-                        >
-                            <span>Your History</span>
-                        </NavLink>
-                    </li>
+                    {sideBarList.map((item) => (
+                        <li key={item.label}>
+                            <NavLink
+                                to={item.link}
+                                className={({ isActive }) =>
+                                    `flex items-center gap-3 px-4 py-3 rounded-lg font-medium text-base transition-all duration-150 ${isActive
+                                        ? "bg-[#2563eb] text-white shadow"
+                                        : "text-[#334155] hover:bg-[#f1f5f9] hover:text-[#2563eb]"
+                                    }`
+                                }
+                            >
+                                <span>{item.label}</span>
+                            </NavLink>
+                        </li>
+                    ))}
                 </ul>
             </nav>
             {/* Logout */}
@@ -87,6 +72,6 @@ export default function Sidebar() {
                     Logout
                 </button>
             </footer>
-        </aside>
+        </aside >
     );
 }
