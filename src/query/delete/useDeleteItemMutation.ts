901ca9143b18a504a36dc7cdac9cd2a0dbafb00c
Christian <christiandave120702@gmail.com>
2025-09-21 11:30:21 +0800

    feat: create a delete item mutation using Tanstack Query

---
+++ src/query/delete/useDeleteItemMutation.ts
@@ -0,0 +1,30 @@
+import { useMutation } from "@tanstack/react-query";
+import { getToken } from "../../utils/token";
+
+const DeleteItem = async (id: string) => {
+  const BASE_URL = import.meta.env.VITE_INVENTORY_LIST_API;
+
+  const res = await fetch(`${BASE_URL}/${id}`, {
+    method: "DELETE",
+    headers: {
+      Authorization: `Bearer ${getToken()}`,
+    },
+    credentials: "include",
+  });
+
+  if (!res.ok) {
+    throw new Error("Failed to delete item");
+  }
+
+  try {
+    return await res.json();
+  } catch {
+    return null;
+  }
+};
+
+export const useDeleteItemMutation = () => {
+  return useMutation<unknown, Error, string>({
+    mutationFn: DeleteItem,
+  });
+};
