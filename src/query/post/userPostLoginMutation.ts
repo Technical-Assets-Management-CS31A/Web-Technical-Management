3b01036ad7936d7359784ac640439a4859cd1c9b
Christian <christiandave120702@gmail.com>
2025-09-26 21:35:00 +0800

    feat: implement usePostLoginMutation for user login handling with token management

---
+++ src/query/post/userPostLoginMutation.ts
@@ -0,0 +1,26 @@
+import { useMutation } from "@tanstack/react-query";
+import { saveToken } from "../../utils/token";
+import type { TLoginUser } from "../../types/types";
+
+const LoginUser = async (formData: TLoginUser) => {
+  const BASE_URL = import.meta.env.VITE_LOGIN_USER_API;
+  const res = await fetch(BASE_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(formData),
+    credentials: "include",
+  });
+  if (!res.ok) throw new Error("Submission failed");
+  const data = await res.json();
+  if (data) saveToken(data.accessToken);
+  return data;
+};
+
+export const usePostLoginMutation = () => {
+  return useMutation({
+    mutationKey: ["login"],
+    mutationFn: LoginUser,
+  });
+};
