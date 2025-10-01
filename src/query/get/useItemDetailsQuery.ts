cdc3adaf4c128bf146654c62d9c593157291ecdd
Christian <christiandave120702@gmail.com>
2025-10-01 12:54:27 +0800

    refactor: update GetItemDetails function to accept string ID, improve API endpoint, and enhance data handling with error logging

--- src/query/get/useItemDetailsQuery.ts
+++ src/query/get/useItemDetailsQuery.ts
@@ -1,22 +1,22 @@
 import { queryOptions } from "@tanstack/react-query";
 import { getToken } from "../../utils/token";
 
-const GetItemDetails = async (id: number) => {
-  const BASE_URL = import.meta.env.VITE_INVENTORY_LIST_API;
+const GetItemDetails = async (id: string) => {
+  const BASE_URL = import.meta.env.VITE_ITEM_DETAILS;
 
   const res = await fetch(`${BASE_URL}/${id}`, {
     method: "GET",
     headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${getToken}`,
+      Authorization: `Bearer ${getToken()}`,
     },
-    credentials: "include",
   });
+  const data = await res.json();
   if (!res.ok) throw new Error("Failed to fetch item details");
-  return res.json();
+  console.log(data);
+  return data;
 };
 
-export const useItemDetailsQuery = (id: number) => {
+export const useItemDetailsQuery = (id: string) => {
   return queryOptions({
     queryKey: ["Item", id],
     queryFn: () => GetItemDetails(id),
