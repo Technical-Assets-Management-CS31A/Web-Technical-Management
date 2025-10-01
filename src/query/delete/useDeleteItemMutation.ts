d43bdefe7abb72c9d80dd3935f094255f06ee62f
Christian <christiandave120702@gmail.com>
2025-10-01 15:16:22 +0800

    refactor: update delete item mutation to use new API endpoint and streamline error handling

--- src/query/delete/useDeleteItemMutation.ts
+++ src/query/delete/useDeleteItemMutation.ts
@@ -2,29 +2,22 @@ import { useMutation } from "@tanstack/react-query";
 import { getToken } from "../../utils/token";
 
 const DeleteItem = async (id: string) => {
-  const BASE_URL = import.meta.env.VITE_INVENTORY_LIST_API;
+  const BASE_URL = import.meta.env.VITE_ITEM_DELETE;
 
   const res = await fetch(`${BASE_URL}/${id}`, {
     method: "DELETE",
     headers: {
       Authorization: `Bearer ${getToken()}`,
     },
-    credentials: "include",
   });
-
-  if (!res.ok) {
-    throw new Error("Failed to delete item");
-  }
-
-  try {
-    return await res.json();
-  } catch {
-    return null;
-  }
+  const data = await res.json();
+  if (!res.ok) throw new Error("Failed to delete item");
+  return data;
 };
 
 export const useDeleteItemMutation = () => {
   return useMutation({
+    mutationKey: ["Item"],
     mutationFn: DeleteItem,
   });
 };
