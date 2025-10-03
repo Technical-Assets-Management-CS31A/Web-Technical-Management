f31663f5c6cad442d9b2bed418846d8f463ffd2d
Christian <christiandave120702@gmail.com>
2025-10-03 12:40:14 +0800

    style: update shadow effect in DashboardBadges component for improved visual consistency

--- src/components/DashboardBadges.tsx
+++ src/components/DashboardBadges.tsx
@@ -6,12 +6,12 @@ type BadgesProps = {
   data: number;
 };
 
-export default function DashboardBadges({ name, link, data}: BadgesProps) {
+export default function DashboardBadges({ name, link, data }: BadgesProps) {
   return (
     <Link
       key={name}
       to={link}
-      className="flex flex-col bg-white/90 shadow-xl rounded-2xl p-8 items-center justify-center hover:scale-105 hover:shadow-2xl transition-all duration-200 border border-[#e0e7ef]"
+      className="flex flex-col bg-white/90 shadow-md rounded-2xl p-8 items-center justify-center hover:scale-105 hover:shadow-2xl transition-all duration-200 border border-[#e0e7ef]"
     >
       <h2 className="font-semibold text-lg text-[#64748b] mb-2">{name}</h2>
       <p className="text-4xl font-bold text-[#2563eb]">{data}</p>
