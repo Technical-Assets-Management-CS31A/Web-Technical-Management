7be6036ec5eb5b6669591006c64fed8119e91041
Christian <christiandave120702@gmail.com>
2025-10-07 20:40:10 +0800

    refactor: migrate token management from localStorage to Cookies for improved security and consistency

--- src/utils/token/index.tsx
+++ src/utils/token/index.tsx
@@ -1,16 +1,20 @@
+import Cookies from "js-cookie";
 
-export const saveToken = (token: string) => {
-    const key = import.meta.env.VITE_ACCESS_TOKEN_KEY || 'accessToken';
-    localStorage.setItem(key, token);
-}
+// export const saveToken = (token: string) => {
+//     const key = import.meta.env.VITE_ACCESS_TOKEN || "accessToken";
+//     Cookies.set(key, token, {
+//         expires: 1,
+//         secure: true,
+//         sameSite: "strict",
+//     });
+// };
 
-export const getToken = (): string | null => {
-    const key = import.meta.env.VITE_ACCESS_TOKEN_KEY || 'accessToken';
-    const token = localStorage.getItem(key);
-    return token;
-}
+export const getToken = (): string | undefined => {
+    const key = import.meta.env.VITE_ACCESS_TOKEN_KEY;
+    return Cookies.get(key);
+};
 
 export const removeToken = () => {
-    const key = import.meta.env.VITE_ACCESS_TOKEN_KEY || 'accessToken';
-    localStorage.removeItem(key);
-}
+    const key = import.meta.env.VITE_ACCESS_TOKEN_KEY;
+    Cookies.remove(key);
+};
