a2b277eb73f08e24357423e5f037763803ff500f
Christian <christiandave120702@gmail.com>
2025-10-08 22:49:38 +0800

    refactor: remove commented-out saveToken function to clean up token utility

--- src/utils/token/index.tsx
+++ src/utils/token/index.tsx
@@ -1,19 +1,9 @@
 import Cookies from "js-cookie";
 
-// export const saveToken = (token: string) => {
-//     const key = import.meta.env.VITE_ACCESS_TOKEN || "accessToken";
-//     Cookies.set(key, token, {
-//         expires: 1,
-//         secure: true,
-//         sameSite: "strict",
-//     });
-// };
-
 export const getToken = (): string | undefined => {
     const key = import.meta.env.VITE_ACCESS_TOKEN_KEY;
     return Cookies.get(key);
 };
-
 export const removeToken = () => {
     const key = import.meta.env.VITE_ACCESS_TOKEN_KEY;
     Cookies.remove(key);
