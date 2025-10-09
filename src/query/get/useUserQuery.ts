6d6b6b8454d0deb48c7f4456bf9393cc83d3eda7
Christian <christiandave120702@gmail.com>
2025-10-09 20:15:55 +0800

    refactor: update user credentials API endpoint variable for clarity

--- src/query/get/useUserQuery.ts
+++ src/query/get/useUserQuery.ts
@@ -2,7 +2,7 @@ import { getToken } from "../../utils/token";
 import { queryOptions } from "@tanstack/react-query";
 
 const fetchUserData = async () => {
-  const BASE_URL = import.meta.env.VITE_USER_CREDENTIALS;
+  const BASE_URL = import.meta.env.VITE_USER_CREDENTIALS_API;
   const res = await fetch(BASE_URL, {
     method: "GET",
     headers: {
