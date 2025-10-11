0e91475406e1b5ef605208b47bfaec93c9e50aed
Christian <christiandave120702@gmail.com>
2025-10-11 09:55:15 +0800

    feat: enhance useAuth hook to include token refresh logic and loading state management

--- src/auth/useAuth.tsx
+++ src/auth/useAuth.tsx
@@ -1,23 +1,63 @@
 import Cookies from "js-cookie";
+import { useEffect, useState } from "react";
+import { getToken } from "../utils/token";
 
 const getAccessTokenKey = (): string => {
   const key = import.meta.env.VITE_ACCESS_TOKEN_KEY;
-  if (!key) {
-    console.warn(
-      "Environment variable 'VITE_ACCESS_TOKEN_KEY' is undefined. Using fallback key 'accessToken'."
-    );
-    return "accessToken";
-  }
   return key;
+};
+
+const getRefreshAccessToken = async (): Promise<string | null> => {
+  try {
+    const BASE_URL = import.meta.env.VITE_REFRESH_TOKEN_API;
+    const res = await fetch(BASE_URL, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      credentials: "include",
+    });
+
+    const data = await res.json();
+    if (!res.ok) throw new Error(data.message || "refreshToken is failed");
+
+    return data.data;
+
+  } catch (err) {
+    console.error("Failed to refresh token:", err);
+    return null;
+  }
 };
 
 export const useAuth = () => {
   const key = getAccessTokenKey();
-  const token = Cookies.get(key);
+  const [loading, setLoading] = useState(true);
+  const [token, setToken] = useState<string | undefined>(() => getToken());
+  const [isAuthenticated, setIsAuthenticated] = useState(Boolean(token));
+
+  useEffect(() => {
+    const refreshNow = async () => {
+      if (token) {
+        setLoading(false);
+        return;
+      }
+
+      const newToken = await getRefreshAccessToken();
+      if (newToken) {
+        Cookies.set(key, newToken);
+        setToken(newToken);
+        setIsAuthenticated(true);
+      } else {
+        setIsAuthenticated(false);
+      }
+      setLoading(false);
+    };
+
+    refreshNow();
+  }, [token, key]);
 
   return {
     key,
     token,
-    isAuthenticated: Boolean(token),
+    isAuthenticated,
+    loading,
   };
 };
