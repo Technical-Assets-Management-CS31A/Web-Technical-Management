6291682ad58f2c401c57611deda6e81e744e5b62
Christian <christiandave120702@gmail.com>
2025-10-11 10:07:35 +0800

    refactor: improve formatting of loading state in ProtectedRoute component for better readability

--- src/utils/middleware/accessAuth.tsx
+++ src/utils/middleware/accessAuth.tsx
@@ -14,14 +14,17 @@ export const PublicRoute = ({ children }: RouteProps) => {
 export const ProtectedRoute = ({ children }: RouteProps) => {
   const { loading, isAuthenticated } = useAuth();
 
-  if (loading) return <div className="flex items-center justify-center h-screen bg-white">
-    <span className="w-12 h-12 border-4 border-orange-500 border-b-transparent rounded-full inline-block box-border animate-spin"></span>
-  </div>
+  if (loading)
+    return (
+      <div className="flex items-center justify-center h-screen bg-white">
+        <span className="w-12 h-12 border-4 border-orange-500 border-b-transparent rounded-full inline-block box-border animate-spin"></span>
+      </div>
+    );
 
   if (!isAuthenticated) {
     removeToken();
     return <Navigate to="/" replace />;
   }
 
-  return isAuthenticated ? <>{children}</> : <Navigate to="/" replace />;
+  return <>{children}</>;
 };
