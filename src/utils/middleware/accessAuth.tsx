a11db6415450dcef71a27037386703da5df63eac
Christian <christiandave120702@gmail.com>
2025-10-12 08:36:05 +0800

    refactor: improve loading state handling in ProtectedRoute component and remove unused PublicRoute

--- src/utils/middleware/accessAuth.tsx
+++ src/utils/middleware/accessAuth.tsx
@@ -6,20 +6,16 @@ interface RouteProps {
   children: React.ReactNode;
 }
 
-export const PublicRoute = ({ children }: RouteProps) => {
-  const { isAuthenticated } = useAuth()
-  return isAuthenticated ? <Navigate to="/home/dashboard" replace /> : <>{children}</>;
-};
-
 export const ProtectedRoute = ({ children }: RouteProps) => {
   const { loading, isAuthenticated } = useAuth();
 
-  if (loading)
+  if (loading){
     return (
       <div className="flex items-center justify-center h-screen bg-white">
         <span className="w-12 h-12 border-4 border-orange-500 border-b-transparent rounded-full inline-block box-border animate-spin"></span>
       </div>
     );
+  }
 
   if (!isAuthenticated) {
     removeToken();
