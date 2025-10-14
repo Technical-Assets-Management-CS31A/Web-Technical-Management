21fd91a25e51a01815583814892abf44c606aafa
Christian <christiandave120702@gmail.com>
2025-10-14 18:04:14 +0800

    fix: update error message in deleteItem function for clarity

--- src/query/delete/useDeleteItemMutation.ts
+++ src/query/delete/useDeleteItemMutation.ts
@@ -3,18 +3,16 @@ import { getToken } from "../../utils/token";
 
 const deleteItem = async (id: string) => {
   const BASE_URL = import.meta.env.VITE_ITEM_DELETE_API
-  const itemId = JSON.stringify(id)
   const res = await fetch(`${BASE_URL}/${id}`, {
     method: "DELETE",
     headers: {
       Authorization: `Bearer ${getToken()}`,
     },
-    body: itemId,
   });
   const data = await res.json();
 
   if (!res.ok) {
-    throw new Error(data.message || "Failed to restore item");
+    throw new Error(data.message || "Failed to delete item");
   }
 
   return data;
