436b554246bb933d474fffed76504186eb0a3cce
Christian <christiandave120702@gmail.com>
2025-10-16 22:11:19 +0800

    refactor: update token refresh logic in useAuth to utilize a base URL and endpoint for improved API structure

--- src/auth/useAuth.tsx
+++ src/auth/useAuth.tsx
@@ -3,16 +3,18 @@ import { useEffect, useState } from "react";
 import { getToken } from "../utils/token";
 
 const getAccessTokenKey = () => import.meta.env.VITE_ACCESS_TOKEN_KEY;
-const BASE_URL = import.meta.env.VITE_REFRESH_TOKEN_API;
 
 const getRefreshAccessToken = async (): Promise<string | null> => {
   try {
+    const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+    const END_POINT = "/api/v1/auth/refresh-token";
+
     if (!BASE_URL) {
       console.error("Refresh token URL not found");
       return null;
     }
 
-    const res = await fetch(BASE_URL, {
+    const res = await fetch(`${BASE_URL}${END_POINT}`, {
       method: "POST",
       credentials: "include",
     });
