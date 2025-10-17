cec72614bab4503a85113d4751a1f93ee0c7859f
Christian <christiandave120702@gmail.com>
2025-10-17 10:55:08 +0800

    refactor: update API endpoint in useRestoreItemMutation for improved structure and consistency

--- src/query/delete/useRestoreItemMutation.ts
+++ src/query/delete/useRestoreItemMutation.ts
@@ -2,14 +2,14 @@ import { useMutation } from "@tanstack/react-query";
 import { getToken } from "../../utils/token";
 
 const restoreItem = async (id: string) => {
-  const BASE_URL = import.meta.env.VITE_RESTORE_ITEMS_API
-  const itemId = JSON.stringify(id)
-  const res = await fetch(`${BASE_URL}/${id}`, {
+  const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+  const END_POINT = "/api/v1/archiveitems/restore";
+
+  const res = await fetch(`${BASE_URL}${END_POINT}/${id}`, {
     method: "DELETE",
     headers: {
       Authorization: `Bearer ${getToken()}`,
     },
-    body: itemId,
   });
   const data = await res.json();
 
