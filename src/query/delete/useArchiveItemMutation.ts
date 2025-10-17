f864147b04160526c8f9f73a2dc06ae8207d6059
Christian <christiandave120702@gmail.com>
2025-10-17 10:53:49 +0800

    refactor: update API endpoint structure in useArchiveItemMutation to improve clarity and maintainability

--- src/query/delete/useArchiveItemMutation.ts
+++ src/query/delete/useArchiveItemMutation.ts
@@ -2,9 +2,10 @@ import { useMutation } from "@tanstack/react-query";
 import { getToken } from "../../utils/token";
 
 const ArchiveItem = async (id: string) => {
-  const BASE_URL = import.meta.env.VITE_ARCHIVE_API;
+  const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+  const END_POINT = "/api/v1/items/archive";
 
-  const res = await fetch(`${BASE_URL}${id}`, {
+  const res = await fetch(`${BASE_URL}${END_POINT}${id}`, {
     method: "DELETE",
     headers: {
       Authorization: `Bearer ${getToken()}`,
