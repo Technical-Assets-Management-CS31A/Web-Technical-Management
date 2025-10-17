c0074e59d82827071b915764e123c8ee3bf3e923
Christian <christiandave120702@gmail.com>
2025-10-17 10:56:04 +0800

    refactor: update API endpoint in useArchivesQuery for improved structure and error handling

--- src/query/get/useArchivesQuery.ts
+++ src/query/get/useArchivesQuery.ts
@@ -2,19 +2,26 @@ import { queryOptions } from "@tanstack/react-query";
 import { getToken } from "../../utils/token";
 
 const ArchivesTable = async () => {
-  const BASE_URL = import.meta.env.VITE_ARCHIVES_DATA_API;
-  const res = await fetch(BASE_URL, {
-    method: "GET",
-    headers: {
-      Authorization: `Bearer ${getToken()}`,
-    },
-  });
+  try {
+    const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+    const END_POINT = "/api/v1/archiveitems";
+    
+    const res = await fetch(`${BASE_URL}${END_POINT}`, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${getToken()}`,
+      },
+    });
 
-  const data = await res.json();
-  if (!res.ok) {
-    throw new Error(data.message || "Archives Data not found");
+    const data = await res.json();
+    if (!res.ok) {
+      throw new Error(data.message || "Archives Data not found");
+    }
+    return data.data;
+
+  } catch (error) {
+    console.log(error)
   }
-  return data.data;
 };
 
 export const useArchivesQuery = () => {
