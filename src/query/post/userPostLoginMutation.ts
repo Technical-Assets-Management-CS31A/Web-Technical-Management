6c0bd1671d6e4ea8e8cdfa32b29c7a9a05afda4d
Christian <christiandave120702@gmail.com>
2025-10-17 10:57:56 +0800

    refactor: update API endpoint in userPostLoginMutation for improved error handling and structure

--- src/query/post/userPostLoginMutation.ts
+++ src/query/post/userPostLoginMutation.ts
@@ -2,22 +2,30 @@ import { useMutation } from "@tanstack/react-query";
 import type { TLoginUser } from "../../types/types";
 
 const LoginUser = async (formData: TLoginUser) => {
-  const BASE_URL = import.meta.env.VITE_LOGIN_USER_API;
-  const userFormData = JSON.stringify(formData)
-  const res = await fetch(BASE_URL, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: userFormData,
-    credentials: "include",
-  });
-  const data = await res.json();
 
-  if (!res.ok) {
-    throw new Error(data.message || "Failed to login user");
+  try {
+    const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+    const END_POINT = "/api/v1/auth/login";
+
+    const userFormData = JSON.stringify(formData)
+    const res = await fetch(`${BASE_URL}${END_POINT}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: userFormData,
+      credentials: "include",
+    });
+    const data = await res.json();
+
+    if (!res.ok) {
+      throw new Error(data.message || "Failed to login user");
+    }
+    return data;
+
+  } catch (error) {
+    console.log(error);
   }
-  return data;
 };
 
 export const usePostLoginMutation = () => {
