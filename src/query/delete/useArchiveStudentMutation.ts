5886b0d3d60f142b786095469229a02fbb5914de
Christian <christiandave120702@gmail.com>
2025-10-18 14:07:24 +0800

    feat: add useArchiveStudentMutation for archiving students with API integration

---
+++ src/query/delete/useArchiveStudentMutation.ts
@@ -0,0 +1,32 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { getToken } from "../../utils/token";
+
+const archiveStudent = async (studentId: string) => {
+  const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+  const END_POINT = "/api/v1/users";
+
+  const res = await fetch(`${BASE_URL}${END_POINT}/${studentId}`, {
+    method: "DELETE",
+    headers: {
+      Authorization: `Bearer ${getToken()}`,
+    },
+  });
+
+  const data = await res.json();
+
+  if (!res.ok) throw new Error(data.message || "Error archiving student");
+
+  return data;
+};
+
+export const useArchiveStudentMutation = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: archiveStudent,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["students"] });
+      queryClient.invalidateQueries({ queryKey: ["users"] });
+    },
+  });
+};
